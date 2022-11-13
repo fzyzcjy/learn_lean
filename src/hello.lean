3a2023e816f24612e9c9d39366c1442ec99ed58a
fzyzcjy <ch271828n@outlook.com>
2022-11-13 21:06:59 +0800

    copy tut

--- src/hello.lean
+++ src/hello.lean
@@ -1,3 +1,8 @@
-import topology.basic
-
-#check topological_space
+example : 3 = 3 :=
+begin
+  generalize : 3 = x,
+  -- goal is x : ℕ ⊢ x = x,
+  revert x,
+  -- goal is ⊢ ∀ (x : ℕ), x = x
+  intro y, reflexivity
+end
